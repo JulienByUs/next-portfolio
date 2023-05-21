175373caa586cb78808caf1aeec282491c9200eb
JulienByUs <82035961+JulienByUs@users.noreply.github.com>
2023-05-21 12:25:39 +0200

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,22 +24,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function HomePage() {
 
-  const featuredPost = {
-    id: 1,
-    title: 'We’re incredibly proud to announce we have secured $75m in Series B',
-    href: '#',
-    description:
-        'Libero neque aenean tincidunt nec consequat tempor. Viverra odio id velit adipiscing id. Nisi vestibulum orci eget bibendum dictum. Velit viverra posuere vulputate volutpat nunc. Nunc netus sit faucibus.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    author: {
-      name: 'Michael Foster',
-      href: '#',
-      imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  }
-
   interface Article {
     id: number;
     title: string;
