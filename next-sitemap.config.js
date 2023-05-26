595ecfb1fa8c7b3ee08d962eaef7c8bf62a54f0e
JulienByUs <82035961+JulienByUs@users.noreply.github.com>
2023-05-26 16:03:38 +0200

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,7 @@
 module.exports = {
   // !STARTERCONF Change the siteUrl
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://www.julienestanis.fr/',
+  siteUrl: 'https://blog.julienestanis.fr/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
