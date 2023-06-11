906cd96bdaff69aabb0a6afc28016a0b1654d1c0
JulienByUs <82035961+JulienByUs@users.noreply.github.com>
2023-06-11 20:26:56 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,37 +19,53 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     useEffect(() => {
 
-        // UPDATE: I was able to get this working again... Enjoy!
-
-        const cursor = document.querySelector('.cursor');
-        const cursorinner = document.querySelector('.cursor2');
         const a = document.querySelectorAll('a');
 
         document.addEventListener('mousemove', function(e) {
-            cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+            const cursor = document.querySelector('.cursor') as HTMLElement; // Get the cursor element
+            if (cursor) {
+                cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+            }
         });
 
         document.addEventListener('mousemove', function(e) {
-            cursorinner.style.left = e.clientX + 'px';
-            cursorinner.style.top = e.clientY + 'px';
+            const cursorinner = document.querySelector('.cursor2') as HTMLElement;
+            if (cursorinner) {
+                cursorinner.style.left = e.clientX + 'px';
+                cursorinner.style.top = e.clientY + 'px';
+            }
         });
 
         document.addEventListener('mousedown', function() {
-            cursor.classList.add('click');
-            cursorinner.classList.add('cursorinnerhover');
+            const cursor = document.querySelector('.cursor') as HTMLElement; // Get the cursor element
+            const cursorinner = document.querySelector('.cursor2') as HTMLElement;
+            if (cursorinner && cursor) {
+                cursor.classList.add('click');
+                cursorinner.classList.add('cursorinnerhover');
+            }
         });
 
         document.addEventListener('mouseup', function() {
-            cursor.classList.remove('click');
-            cursorinner.classList.remove('cursorinnerhover');
+            const cursor = document.querySelector('.cursor') as HTMLElement; // Get the cursor element
+            const cursorinner = document.querySelector('.cursor2') as HTMLElement;
+            if (cursorinner && cursor) {
+                cursor.classList.remove('click');
+                cursorinner.classList.remove('cursorinnerhover');
+            }
         });
 
         a.forEach(item => {
             item.addEventListener('mouseover', () => {
-                cursor.classList.add('hover');
+                const cursor = document.querySelector('.cursor') as HTMLElement; // Get the cursor element
+                if (cursor) {
+                    cursor.classList.add('hover');
+                }
             });
             item.addEventListener('mouseleave', () => {
-                cursor.classList.remove('hover');
+                const cursor = document.querySelector('.cursor') as HTMLElement; // Get the cursor element
+                if (cursor) {
+                    cursor.classList.remove('hover');
+                }
             });
         });
 
